dade414e6dad3bae1f358f6132f30858a6958c8e
Doug Turner <doug.turner@gmail.com>
2015-06-05 08:41:51 -0700

    Use serviceWorker.ready.then before calling subscribe

--- src/githubpusher/frontend/static/install.js
+++ src/githubpusher/frontend/static/install.js
@@ -1,22 +1,25 @@
 
 function register() {
-  navigator.serviceWorker.register("sw.js", {scope: '/'}).then(
-    function(serviceWorkerRegistration) {
-      serviceWorkerRegistration.pushManager.subscribe().then(
+  navigator.serviceWorker.register("sw.js", {scope: '/'});
+
+  navigator.serviceWorker.ready.then(
+    function(swr) {
+
+      swr.pushManager.subscribe().then(
         function(pushSubscription) {
-          console.log(pushSubscription.endpoint);
 
           var data = new FormData();
           data.append('endpoint', pushSubscription.endpoint);
           var xhr = new XMLHttpRequest();
           xhr.open("POST", "/register");
           xhr.onload = function () {
-            console.log(this.responseText);
             window.location = "/manage"
           };
           xhr.send(data);
 
-        }, function(error) {
+        },
+
+        function(error) {
           console.log(error);
         }
       );
